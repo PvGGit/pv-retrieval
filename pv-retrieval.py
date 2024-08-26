89cd07c0ebc20aeece68842e4ee416ce7fe0de79
Paul van Geldrop <git@paulvangeldrop.nl>
2024-08-26 16:42:17 +0000

    --retrieve-pvcs function finished

--- pv-retrieval.py
+++ pv-retrieval.py
@@ -101,9 +101,12 @@ def retrieve_pvcs_from_clusters(kube_config, target, source_context, target_cont
       bound_source_volumes = [source_volume for source_volume in source_volumes.items if source_volume.status.phase=='Bound']
       # Now let's write namespace:pvc-name into a file for every remaining PV
       with open('source_pvcs.txt', 'w') as f:
-        f.write('Source PVCs:\n')
+        f.write(f'PVCs for source-context {source_context}:\n')
         for volume in bound_source_volumes:
           f.write(f'{volume.spec.claim_ref.namespace}:{volume.spec.claim_ref.name}\n') 
+      
+      # Inform the user the file has been written
+      print(f'PVCs for source-context {source_context} written to source_pvcs.txt')
       
     # Retrieve the PVs from the target-context
     if (target=='target' or target=='both'):
@@ -113,6 +116,14 @@ def retrieve_pvcs_from_clusters(kube_config, target, source_context, target_cont
       target_volumes = v1.list_persistent_volume()
       # We'll ignore PVs that are not set to Bound
       bound_target_volumes = [target_volume for target_volume in target_volumes.items if target_volume.status.phase=='Bound']
+      # Now let's write namespace:pvc-name into a file for every remaining PV
+      with open('target_pvcs.txt', 'w') as f:
+        f.write(f'PVCs for target-context {target_context}:\n')
+        for volume in bound_target_volumes:
+          f.write(f'{volume.spec.claim_ref.namespace}:{volume.spec.claim_ref.name}\n')
+      
+      # Inform the user the file has been written
+      print(f'PVCs for target-context {target_context} written to target_pvcs.txt')
 
 
 # Function to retrieve PVs from both clusters and match them together
