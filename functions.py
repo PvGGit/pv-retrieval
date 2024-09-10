8f1cab89aa42a2e8c2839db1c8246b61f959dbac
Paul van Geldrop <p.j.vangeldrop@tilburguniversity.edu>
2024-09-10 12:03:15 +0200

    Cleaned up code

--- functions.py
+++ functions.py
@@ -7,57 +7,18 @@ from kubernetes import (
 from kubernetes.client.rest import ApiException
 import re
 
-
 # Function to retrieve KUBECONFIG from the environment variables if present
 def retrieve_kubeconfig_env() -> str:
     try:
         return os.environ['KUBECONFIG']
     except KeyError:
-        raise RuntimeError('No KUBECONFIG found in environment variables')
-
-# Function to check cluster connectivity. Listing persistent volumes is required for this script to work.
-def check_context_connectivity(kube_config, context):
-
-    # Get config loaded up and extract contexts and active_context from the config
-    config.load_kube_config(config_file=kube_config)
-    contexts, active_context = config.list_kube_config_contexts()
-
-    # Are there contexts in the kubeconfig file?
-    if not contexts:
-        print('No contexts found in kubeconfig')
-        return False
-
-    # Is the context (either passed or from active_context) present in the kubeconfig file?
-    contexts = [item['name'] for item in contexts]
-    if context not in contexts:
-        print(f'The context {context} was not found in the kubeconfig file')
-        return False
-
-    # If the passed context is present, try to list persistentvolumes.
-    else:
-        v1 = client.CoreV1Api(api_client=config.new_client_from_config(context=context))
-        # Try to get the persistent volumes
-        try:
-            pvs = v1.list_persistent_volume()
-            return True
-        except:
-            print(
-                f'Error connecting to context {context}. Make sure your user has the permission to list persistent volumes.'
-            )
-            return False
-
+        raise RuntimeError('No KUBECONFIG found in environment variables, please specify a valid kube-config file')
 
 # Function to list existing PersistentVolumes from a cluster
 def list_pvs(kube_config, context):
     config.load_kube_config(config_file=kube_config)
     v1 = client.CoreV1Api(api_client=config.new_client_from_config(context=context))
-    # List PersistentVolumes and return if found
-    persistent_volumes = v1.list_persistent_volume()
-    if persistent_volumes:
-        return persistent_volumes
-    else:
-        return False
-
+    return v1.list_persistent_volume()
 
 # Function to retrieve bound PVCs in a cluster
 def retrieve_pvcs_from_clusters(
@@ -135,9 +96,8 @@ def retrieve_pvcs_from_clusters(
 # Function to retrieve the active context from kube-config
 def retrieve_source_context(kube_config):
     config.load_kube_config(config_file=kube_config)
-    contexts, active_context = config.list_kube_config_contexts()
+    _, active_context = config.list_kube_config_contexts()
     return active_context['name']
-
 
 # Function to retrieve PVs from both clusters and match them together
 def retrieve_pvs(kube_config, source_context, target_context):
@@ -165,8 +125,7 @@ def retrieve_pvs(kube_config, source_context, target_context):
             for item in source_pvs_full.items
             if item.status.phase == 'Bound'
         ]
-        # Print statement below for debugging purposes
-        # print(f'source_pvs is populated with: {source_pvs}')
+
         target_pvs_full = list_pvs(kube_config, target_context)
         # If PVs were returned for target-context, we call the matching function
         if target_pvs_full:
@@ -192,25 +151,19 @@ def retrieve_pvs(kube_config, source_context, target_context):
                 for item in target_pvs_full.items
                 if item.status.phase == 'Bound'
             ]
-            # Print statement below for debugging purposes
-            # print(f'target_pvs is populated with {target_pvs}')
 
             # Now that we have both dicts populated, it's time to match them
             match_pvs(source_pvs, target_pvs)
         else:
-            print(f'No PersistentVolumes were found in context {target_context}')
-            sys.exit(1)
+            raise RuntimeError(f'No PersistentVolumes were found in context {target_context}')
     else:
-        print(f'No PersistentVolumes were found in context {source_context}')
-        sys.exit(1)
+        raise RuntimeError(f'No PersistentVolumes were found in context {source_context}')
 
 
 # Function to match the retrieved PVs from source and target cluster in the situation where PVCs are named identically and in the same namespace (exact copy)
 def match_pvs(source_pvs, target_pvs):
     matched_pvs = []
-    # Debugging prints
-    # print(source_pvs)
-    # print(target_pvs)
+
     # Loop through the source_pvs list and match pvc_name and pvc_ns onto target_pvs
     for source_item in source_pvs:
         matching_target_item = next(
@@ -239,15 +192,20 @@ def match_pvs(source_pvs, target_pvs):
                 f"Data dirs: {item['source_pv']['data_dir']} {item['target_pv']['data_dir']}"
             )
     else:
-        print(
+        raise RuntimeError(
             'PVCs in source and target cluster are not identical. Please supply a mapping file using the --mapping-file parameter instead.'
         )
 
+# Simple function to select PVs based on properties
+def select_pv_on_pvc(pv_list, ns, pvc_name):
+    for pv in pv_list:
+        if pv.spec.claim_ref.namespace == ns and pv.spec.claim_ref.name == pvc_name:
+            return pv
 
-# Function to check the validity of a supplied mapping file
-def is_valid_mapping_file(mapping_file, kube_config, source_context, target_context):
-
-    # Does every line in the file match the structure we expect?
+def retrieve_dirs_from_mapping_file(
+    mapping_file, kube_config, source_context, target_context
+):
+    # Do entries in the mapping file conform to the expected pattern?
     pattern = r'[a-z0-9]([-a-z0-9]*[a-z0-9])?'
     full_pattern = re.compile(rf'^{pattern}:{pattern},{pattern}:{pattern}$')
 
@@ -255,10 +213,9 @@ def is_valid_mapping_file(mapping_file, kube_config, source_context, target_cont
         for line_number, line in enumerate(file, start=1):
             line = line.strip()
             if not full_pattern.match(line):
-                print(
+                raise RuntimeError(
                     f"Error found in line {line_number} in mapping file. Lines should consist of namespace:pvc-name,namespace:pvc-name only. Namespace and PVCs should adhere to Kubernetes naming conventions."
                 )
-                return False
 
     # Now let's check if the PVCs passed in the mapping file actually exist in the cluster
     # Let's first split the mapping file into source and target PVCs.
@@ -270,10 +227,6 @@ def is_valid_mapping_file(mapping_file, kube_config, source_context, target_cont
             mapping_file_source_pvc, mapping_file_target_pvc = line.split(',')
             mapping_file_source_pvcs.append(mapping_file_source_pvc)
             mapping_file_target_pvcs.append(mapping_file_target_pvc)
-    # Debugging print
-    # print(mapping_file_source_pvcs)
-    # print(mapping_file_target_pvcs)
-
     # Now let's retrieve the PVCs using the retrieve_pvcs() function. We pass no_output as True this time to ensure it doesn't overwrite any existing files
     cluster_source_pvcs = retrieve_pvcs_from_clusters(
         kube_config, 'source', source_context, target_context, True
@@ -285,30 +238,15 @@ def is_valid_mapping_file(mapping_file, kube_config, source_context, target_cont
     # Now that we have the PVCs existing in the cluster, let's check the data we were given in the mappingfile.
     for item in mapping_file_source_pvcs:
         if item not in cluster_source_pvcs:
-            print(
+            raise RuntimeError(
                 f'Source PVC {item} from mapping file was not found in the source cluster'
             )
-            return False
 
     for item in mapping_file_target_pvcs:
         if item not in cluster_target_pvcs:
-            print(f'Target PVC {item} was not found in source cluster')
-            return False
+            raise RuntimeError(f'Target PVC {item} was not found in source cluster')
 
-    # We're all set to go!
-    return True
-
-
-# Simple function to select PVs based on properties
-def select_pv_on_pvc(pv_list, ns, pvc_name):
-    for pv in pv_list:
-        if pv.spec.claim_ref.namespace == ns and pv.spec.claim_ref.name == pvc_name:
-            return pv
-
-
-def retrieve_dirs_from_mapping_file(
-    mapping_file, kube_config, source_context, target_context
-):
+    # Now that we know the file is valid and the PVCs are correct, let's get to work
     # List PVs from source cluster
     config.load_kube_config(config_file=kube_config)
     source_v1 = client.CoreV1Api(
@@ -350,5 +288,4 @@ def retrieve_dirs_from_mapping_file(
                 )
 
     else:
-        print('No bound PVs found in source or target cluster')
-    return True
+        raise RuntimeError('No bound PVs found in source or target cluster')
